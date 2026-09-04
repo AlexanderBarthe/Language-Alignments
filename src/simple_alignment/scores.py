96252d118d4c9d280804bfccf42fffa2f588a435
AlexanderBarthe <dev@orangen-limona.de>
2026-09-04 11:19:55 +0200

    Update lingpy scoring function

--- src/simple_alignment/scores.py
+++ src/simple_alignment/scores.py
@@ -112,34 +112,19 @@ class AlignmentScorer:
         return best_score, best_syllable_length
 
     def get_lingpy_comparison_score(self, char1: str, char2: str) -> float:
-        lp = CONFIG["lingpy"]
-
         class1 = self.model.converter.get(char1, char1)
         class2 = self.model.converter.get(char2, char2)
 
-        # determine base raw score from model or exact match
-        if char1 == char2:
-            raw_score = lp['exact_match_score']
-        else:
-            raw_score = self.model.scorer[class1, class2]
-            if raw_score >= lp["threshold_high"]:
-                raw_score = lp["score_high"]
-            elif raw_score >= lp["threshold_mid"]:
-                raw_score = lp["score_mid"]
-            elif raw_score >= lp["threshold_low"]:
-                raw_score = lp["score_low"]
-            else:
-                raw_score = lp["score_mismatch"]
+        raw_score = float(self.model.scorer[class1, class2])
 
-        # adjust raw score using calculated lexstat matrix if available
-        if self.lexstat_matrix is not None:
-            pair = (class1, class2)
-            lexstat_weight = CONFIG["alignment"].get("lexstat_weight", 0.5)
+        if self.lexstat_matrix is None:
+            return raw_score
 
-            ls_score = self.lexstat_matrix.get(pair, 0.0)
-            return (1-lexstat_weight)*raw_score + (lexstat_weight * ls_score)
+        pair = (class1, class2)
+        lexstat_weight = float(CONFIG["alignment"].get("lexstat_weight", 0.5))
+        ls_score = float(self.lexstat_matrix.get(pair, 0.0))
 
-        return raw_score
+        return (1.0 - lexstat_weight) * raw_score + (lexstat_weight * ls_score)
 
     def get_lingpy_string_score(self, str1: str, str2: str) -> float:
         accu = 0
